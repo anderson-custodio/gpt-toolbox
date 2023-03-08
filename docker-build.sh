fbb5550e0d79cf30fe834e1e9d200927500931f9
Anderson Fernando Custódio <anderson.custodio@luizalabs.com>
2023-03-08 11:10:09 -0300

    feat: add docker publish

--- docker-build.sh
+++ docker-build.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-api_key=$1
-org=$2
-docker build -t meu-app-react --build-arg REACT_APP_OPENAI_KEY="${api_key}" --build-arg REACT_APP_ORGANIZATION_ID="${org}" .
+docker build -t afcustodioo/gpt-toolbox .
